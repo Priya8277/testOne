b6da4af65b3633b98f3ee507f8ae38705bd440b3
Priya <priya.nandu18@gmail.com>
2019-01-29 11:22:49 +0530

    first commit

---
+++ TabBarApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBarApp
+//
+//  Created by Priya on 1/29/19.
+//  Copyright © 2019 Priya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
